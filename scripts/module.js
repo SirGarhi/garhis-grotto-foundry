4c24f87f7c1e959abeae19823961d76fb16d072f
SirGarhi <113884133+SirGarhi@users.noreply.github.com>
2023-10-21 21:08:20 -0700

    Update to V11

--- scripts/module.js
+++ scripts/module.js
@@ -4,6 +4,47 @@ import {effects} from './effects.js';
 import {ggHelpers} from './helperFunctions.js';
 
 async function createWorldActors() {
+    let folder = game.folders.getName('GG - Actor Templates');
+    if (!folder) {
+        folder = await Folder.create({
+            'name': 'GG - Actor Templates',
+            'type': 'Actor',
+            'color': '#666666'
+        });
+    }
+    let actorsCompendium = game.packs.get('garhis-grotto.gg-actor-blueprints');
+    if (!actorsCompendium) return;
+    let documents = await actorsCompendium.getDocuments();
+    if (documents.length === 0) return;
+    for (let actor of documents) {
+        let folderActor = folder.contents.find(act => act.name === actor.name);
+		if (!folderActor) {
+			let actorData = actor.toObject();
+        	actorData.folder = folder.id;
+			await Actor.create(actorData);
+		}
+		/*
+        let avatarImg;
+        let tokenImg;
+        let imageFlags;
+        if (folderActor) {
+            let folderVersion = folderActor.flags['chris-premades']?.version;
+            let documentVersion = actor.flags['chris-premades']?.version;
+            if (folderVersion && folderVersion === documentVersion) continue;
+            avatarImg = folderActor.img;
+            tokenImg = folderActor.prototypeToken.texture.src;
+            imageFlags = folderActor.flags['chris-premades']?.summon;
+            await folderActor.delete();
+        }
+        let actorData = actor.toObject();
+        actorData.folder = folder.id;
+        if (avatarImg) actorData.img = avatarImg;
+        if (tokenImg) actorData.prototypeToken.texture.src = tokenImg;
+        if (imageFlags) actorData.flags['chris-premades'].summon = imageFlags;
+        await Actor.create(actorData);
+		*/
+    }
+	/*
 	let spiritualWeapon = await ggHelpers.getItemFromCompendium('garhis-grotto.gg-actor-blueprints', 'GG - Spiritual Weapon', false, false);
 	if (spiritualWeapon) {
 		let existingActor = game.actors.getName('GG - Spiritual Weapon');
@@ -18,6 +59,7 @@ async function createWorldActors() {
 			await Actor.create(cloudDaggers);
 		}
 	}
+	*/
 }
 
 Hooks.once('init', async function() {
